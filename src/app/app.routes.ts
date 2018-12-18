7b215ac727329064e2e013160808233587130e10
Geraldo Costa <geraldocoosta@gmail.com>
2018-12-18 16:06:32 -0200

    componentes novos e mais algumas anotações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from "@angular/router";
 
-export const ROUTES: Routes []
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'restaurants', component: RestaurantsComponent }
+];
