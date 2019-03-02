44e3f20fbbd6333dd973928b58a850c522db0cb9
Geraldo <geraldocoosta@gmail.com>
2019-03-02 19:18:08 -0300

    Após aula 64, colocando itens do form para ele validar

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  paymentOptions: RadioOption[] = [
+    {label:'Dinheiro', value:'MON'},
+    {label:'Cartão de Débito', value:'DEB'},
+    {label:'Cartão Refeição', value:'REF'},
+   ];
 
-  ngOnInit() {
-  }
+constructor() { }
+
+ngOnInit() {
+}
 
 }
---
+++ src/app/shared/radio/radio.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { RadioOption } from './radio-option.model';
+
+@Component({
+  selector: 'mt-radio',
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true,
+    }
+  ]
+})
+export class RadioComponent implements OnInit, ControlValueAccessor {
+
+  onChange: any;
+
+  @Input() options: RadioOption[];
+
+  value: any;
+
+  constructor() { }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  ngOnInit() {
+  }
+
+  setValue(value: any) {
+    this.value = value;
+    this.onChange(this.value);
+  }
+
+}
